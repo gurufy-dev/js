b82a67f3e065ea06bcab10eec9d5e6858ab5d67b
Ilez Kurskiev <ilez.kurskiev@gmail.com>
2023-04-25 18:46:31 +0300

    1

---
+++ 1/example.js
@@ -0,0 +1,20 @@
+// /*
+// Jajdoasjdoiasidasd
+// Jajdoasjdoiasidasd
+// Jajdoasjdoiasidasd
+// Jajdoasjdoiasidasd
+
+// */
+// document.write("asdkasopdkopasdasdasd world");
+
+// let city;
+// document.write(city);
+
+const city = "Magas";
+let age = "30";
+
+age = "31";
+
+city = "New York";
+
+document.write(city);
